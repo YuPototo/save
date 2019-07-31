82fecc43d5cea368c3eb29ba782ee6d5594d073f
Yu Qin <sirius.qinyu@gmail.com>
2019-07-31 17:05:24 +0800

    try catch asyinc code

--- src/store.js
+++ src/store.js
@@ -42,29 +42,33 @@ export default new Vuex.Store({
       }
     },
     async addSaving({commit}, saving) {
-      const newSaving = {...saving, time:Date.now()}
-      const newSavings = _.cloneDeep(state.savings)
-      newSavings.unshift(newSaving)
-      const data =  {
-        username: 'qinyu', 
-        savings: newSavings
-      }
-      await axios.post('http://localhost:3000/', data)
-      commit('ADD_SAVING', newSaving)
+      try {
+        const newSaving = {...saving, time:Date.now()}
+        const newSavings = _.cloneDeep(state.savings)
+        newSavings.unshift(newSaving)
+        const data =  {
+          username: 'qinyu', 
+          savings: newSavings
+        }
+        await axios.post('http://localhost:3000/', data)
+        commit('ADD_SAVING', newSaving)
+      } catch(error) {
+        console.log(error)
+      }  
     },
     async editSaving({commit}, savingToEdit) {
-      const newSavings = _.cloneDeep(state.savings)
-      const index = newSavings.findIndex(
-        saving => saving.time == savingToEdit.time
-      );
-      if (index !== -1) {
-        newSavings.splice(index, 1, savingToEdit);
-      }
-      const data = {
-        username: 'qinyu',
-        savings: newSavings
-      }
       try {
+        const newSavings = _.cloneDeep(state.savings)
+        const index = newSavings.findIndex(
+          saving => saving.time == savingToEdit.time
+        );
+        if (index !== -1) {
+          newSavings.splice(index, 1, savingToEdit);
+        }
+        const data = {
+          username: 'qinyu',
+          savings: newSavings
+        }
         await axios.post('http://localhost:3000/', data)
         commit('EDIT_SAVING', savingToEdit)
       } catch(error) {
@@ -72,16 +76,16 @@ export default new Vuex.Store({
       }
     },
     async removeSaving({commit}, savingToDelete) {
-      const newSavings = _.cloneDeep(state.savings)
-      let index = newSavings.findIndex(
-        saving => saving.time === savingToDelete.time
-      )
-      newSavings.splice(index, 1);
-      const data =  {
-        username: this.$store.state.username, 
-        savings: newSavings
-      }
       try {
+        const newSavings = _.cloneDeep(state.savings)
+        let index = newSavings.findIndex(
+          saving => saving.time === savingToDelete.time
+        )
+        newSavings.splice(index, 1);
+        const data =  {
+          username: this.$store.state.username, 
+          savings: newSavings
+        }
         await axios.post('http://localhost:3000/', data)
         commit('REMOVE_SAVING', savingToDelete)
       } catch(error) {
